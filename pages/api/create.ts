1ef7abfa131a033add19e4d8ddbedf276a194826
Navid Asgharzadeh <navid.asgharzadeh.r@gmail.com>
2022-11-11 01:42:19 +0100

    Finalize create api route

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,11 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Member, Prisma } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { MemberProps } from '../../utility/Interface'
+import { prisma } from '../../utility/db'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<MemberProps>
+  res: NextApiResponse<Member | { error: string }>
 ) {
-  console.log(req)
-  res.status(200).json(req.body)
+  if (req.method === 'POST') {
+    const { name, email, country, phone } = req.body
+    if (!email) return res.status(400).json({ error: 'Email is required' })
+
+    try {
+      const member = await prisma.member.create({
+        data: {
+          name,
+          email,
+          country,
+          phone,
+        },
+      })
+
+      return res.status(200).json(member)
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          return res.status(400).json({ error: 'Email already exists' })
+        }
+      }
+    }
+  }
+
+  return res.status(405).json({ error: 'Method not allowed' })
 }
