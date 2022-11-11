43906173ce885b9663b3eef1932d1246671b1241
Navid Asgharzadeh <navid.asgharzadeh.r@gmail.com>
2022-11-11 01:11:38 +0100

    Change the dashboard UI

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -46,52 +46,56 @@ function Dashboard() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-16 my-8 max-w-6xl mx-auto">
-      <h1 className="text-4xl font-bold">Welcome {name?.toUpperCase()} 👋</h1>
+    <div className="min-h-screen space-y-8">
+      <h1 className="text-4xl text-center font-bold mt-16">
+        Welcome {name?.toUpperCase()} 👋
+      </h1>
+      <div className="flex items-center justify-between  p-8 mx-auto ">
+        <div className="w-1/5">
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+            <Input
+              label="Name"
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
 
-      <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-2/4">
-        <Input
-          label="Name"
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
+            <Input
+              label="Country"
+              type="text"
+              name="country"
+              value={formData.country}
+              onChange={handleChange}
+            />
 
-        <Input
-          label="Country"
-          type="text"
-          name="country"
-          value={formData.country}
-          onChange={handleChange}
-        />
+            <Input
+              label="Phone"
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
 
-        <Input
-          label="Phone"
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
+            <Input
+              label="Email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
 
-        <Input
-          label="Email"
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
+            <Button disabled={isDisabled()}>Add Member</Button>
+          </form>
+        </div>
+        {/* Show this when no member found */}
+        <p className="text-2xl text-center text-gray-300 font-bold">
+          No member found!
+        </p>
 
-        <Button disabled={isDisabled()}>Add Member</Button>
-      </form>
-
-      {/* Show this when no member found */}
-      <p className="text-2xl text-center text-gray-300 font-bold">
-        No member found!
-      </p>
-
-      <div className="grid gap-8 space-x-1 md:grid-cols-2 lg:grid-cols-3">
-        {/*TODO: Show members */}
+        <div className="grid gap-8 space-x-1 md:grid-cols-2 lg:grid-cols-3">
+          {/*TODO: Show members */}
+        </div>
       </div>
     </div>
   )
