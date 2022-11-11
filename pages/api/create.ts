79634e9569fb70334cfe0832b8def80b08f01c1e
Navid Asgharzadeh <navid.asgharzadeh.r@gmail.com>
2022-11-11 01:12:07 +0100

    Create a very simple endpoint for creating members

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,14 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
+import { MemberProps } from '../../utility/Interface'
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<MemberProps>
 ) {
   console.log(req)
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(req.body)
 }
